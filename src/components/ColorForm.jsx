74cdac3029efceb621b9d67b54cd6f536c58e109
GensengG <168531024+GensengG@users.noreply.github.com>
2025-02-13 00:10:02 +0300

    Update ColorForm.jsx

--- src/components/ColorForm.jsx
+++ src/components/ColorForm.jsx
@@ -1,14 +1,15 @@
-import {useState} from "react";
+import "../App.jsx";
+import { useState } from "react";
 
 export const ColorForm = () => {
 
     let [state, setState] = useState("");
-    let result: string = "";
+    let result = "";
 
     const handleChange = (e) => {
         let valueStart = e.target.value;
         if((valueStart.length === 7) && (valueStart[0] === "#")){
-            let withoutFirst: string = valueStart.slice(1);
+            let withoutFirst = valueStart.slice(1);
             let r = withoutFirst.slice(0,2);
             let g = withoutFirst.slice(2,4);
             let b = withoutFirst.slice(4,6);
